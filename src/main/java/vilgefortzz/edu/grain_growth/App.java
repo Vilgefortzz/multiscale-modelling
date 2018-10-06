0a8f8d33991a3cb720486f90fb4faee1134a9b8d
vilgefortzz <greghause123@gmail.com>
2018-10-06 18:46:30 +0200

    Setup with JavaFX

---
+++ src/main/java/vilgefortzz/edu/grain_growth/App.java
@@ -0,0 +1,17 @@
+package vilgefortzz.edu.grain_growth;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by vilgefortzz on 06/10/18
+ */
+public class App extends Application {
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {}
+}
